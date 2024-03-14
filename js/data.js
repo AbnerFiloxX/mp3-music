bb7bfc4a1714f2d5111e4819d68de6ebf95dea4a
Abner Ivan Angel Camas <satellite15@outlook.es>
2024-03-14 17:37:24 -0600

    para subirlo al netlify

---
+++ js/data.js
@@ -0,0 +1,62 @@
+const songs= [
+    {
+        name: 'Despues de la playa',
+        path: 'music/song1.mp3',
+        artist: 'Bad Bunny',
+        cover:'images/disco1.png'
+    },
+    {
+        name: 'Demons',
+        path: 'music/song2.mp3',
+        artist: 'Imagine Dragon',
+        cover:'images/disco2.png'
+    },
+    {
+        name: 'Fuiste Mala',
+        path: 'music/song3.mp3',
+        artist: 'Kumbia kings',
+        cover:'images/disco3.png'
+    },
+    {
+        name: 'Voy a estar bien',
+        path: 'music/song4.mp3',
+        artist: 'Carin Leon',
+        cover:'images/disco4.png'
+    },
+    {
+        name: 'Alone',
+        path: 'music/song5.mp3',
+        artist: 'Marshmello',
+        cover:'images/disco5.png'
+    },
+    {
+        name: 'Modo Maldito',
+        path: 'music/song6.mp3',
+        artist: 'Fuerza Regida',
+        cover:'images/disco6.png'
+    },
+    {
+        name: 'Lady Gaga',
+        path: 'music/song7.mp3',
+        artist: 'Peso Pluma',
+        cover:'images/disco7.png'
+    },
+    {
+        name: 'No',
+        path: 'music/song8.mp3',
+        artist: 'Alfredo Olivas',
+        cover:'images/disco8.png'
+    },
+    {
+        name: 'Amor Propio',
+        path: 'music/song9.mp3',
+        artist: 'Grupo Frontera',
+        cover:'images/disco9.png'
+    },
+    {
+        name: 'Ya no somos ni seremos',
+        path: 'music/song10.mp3',
+        artist: 'Christian Nodal',
+        cover:'images/disco10.png'
+    }
+]
